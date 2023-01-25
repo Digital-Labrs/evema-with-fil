d948940dfe2667857d748f716746148fb6dc24c8
Techydom <drmusic2021@gmail.com>
2023-01-25 11:10:57 +0100

    evema main

--- config/networks.ts
+++ config/networks.ts
@@ -3,13 +3,13 @@ import filABI from './evemafil.json'
 
 export const NETWORKS: any = {
     polygon_mumbai: {
-        ca: "0x566C27A4953B13AF8388E45F26381c06A4D33B8e",
+        ca: "0xc3a06FbB1477dE3bC24a75A1807151d43eC6A913",
         abi: mumbaiABI,
         chainId: 80001,
         title: 'Mumbai'
     },
     fil_testnet: {
-        ca: "0x67C00F69F2D5a8671A0C4B3bfccE79446bfC6911",
+        ca: "0x05eC409dD081405659F104e68C2e0686Ed1146d8",
         abi: filABI,
         chainId: 3141,
         title: 'Filecoin'
